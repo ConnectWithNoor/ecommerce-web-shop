5c7ffe2e052f31ee5527b27d72c0cd568e0118c9
Noor Muhammad <connectwithnoor1@gmail.com>
2021-12-05 17:14:52 +0500

    connected commerce.js and added products

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
 import { Products, Navbar } from './components';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await commerce.products.list();
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <Navbar />
-      <Products />
+      <Products productsList={products} />
     </div>
   );
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,33 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 
 import Product from './Product/Product';
-
 import useStyles from './styles';
 
-const productList = [
-  {
-    id: 1,
-    name: 'Shoes',
-    description: 'Running Shoes',
-    price: '$5',
-  },
-  {
-    id: 2,
-    name: 'iPhone',
-    description: 'Running iPhone',
-    price: '$10',
-  },
-];
-
-function Products() {
+function Products({ productsList }) {
   const classes = useStyles();
 
   return (
     <main classes={classes.content}>
       <div className={classes.toolbar} />
       <Grid container justify='center' spacing={4}>
-        {productList.map((p) => (
+        {productsList.map((p) => (
           <Grid item key={p.id} xs={12} sm={6} md={4} lg={3}>
             <Product product={p} />
           </Grid>
@@ -36,5 +21,20 @@ function Products() {
     </main>
   );
 }
+
+Products.propTypes = {
+  productsList: PropTypes.instanceOf(Array),
+};
+
+Products.defaultProps = {
+  productsList: [
+    {
+      id: '1',
+      name: 'dummy',
+      price: '$1.0',
+      description: 'dummy description',
+    },
+  ],
+};
 
 export default Products;
